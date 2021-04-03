33f15fae53fb164de89cf8c3c4be2e929284917b
CaseyWalter <you@example.com>
2021-04-02 23:24:20 -0700

    git ignore and cross compatibility

---
+++ Casey's first project/src/Picker/pick.java
@@ -0,0 +1,24 @@
+package Picker;
+
+import java.util.Scanner;
+
+public class pick {
+	int score1;
+	int score2;
+	int score3;
+	public void main(String agrs[]) 
+	{
+		System.out.println("Throat?\nType 1 for yes\nType 2 for no");
+		Scanner scan = new Scanner(System.in);
+		if(scan.nextInt() == 1) 
+		{
+			System.out.println("Here you go!\nhttps://www.pornhub.com/video/search?search=throat+fuck");
+		} 
+		System.out.println("Dillon?\nType 1 for yes\nType 2 for no");
+		if(scan.nextInt() == 0)
+		{
+			System.out.println("Here you go!\nhttps://www.pornhub.com/video/search?search=dillon+harper&correction=false");
+		}
+		scan.close();
+	}
+}
